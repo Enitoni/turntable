a449ecbdf67e2d3d37bf8d277ba200a16f020be1
Enitoni <5295397+Enitoni@users.noreply.github.com>
2023-05-04 07:06:12 +0200

    Add ApiError type for common errors

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,70 @@
+use axum::response::IntoResponse;
+use hyper::StatusCode;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ApiError {
+    #[error("{0} does not exist")]
+    NotFound(&'static str),
+
+    #[error("{0} already exists")]
+    Conflict(&'static str),
+
+    #[error(transparent)]
+    Database(#[from] surrealdb::Error),
+
+    #[error("{0}")]
+    Other(Box<dyn std::error::Error>),
+
+    #[error("Unknown error")]
+    Unknown,
+}
+
+use surrealdb::error::Api as ApiErr;
+use surrealdb::error::Db as DbErr;
+use surrealdb::Error as SurrealErr;
+
+impl ApiError {
+    pub fn from_db(err: SurrealErr) -> Self {
+        dbg!(&err);
+
+        match err {
+            SurrealErr::Db(x) => match x {
+                DbErr::RecordExists { thing: _ } => Self::Conflict("Resource"),
+                x => Self::Database(SurrealErr::Db(x)),
+            },
+            SurrealErr::Api(err) => Self::from_db_api(err),
+        }
+    }
+
+    fn from_db_api(err: ApiErr) -> Self {
+        match err {
+            ApiErr::Query(x) => {
+                // Sadly, SurrealDB does not have a proper error for this
+                // when interacting via a remote instance.
+                if x.contains("already exists") {
+                    return Self::Conflict("Resource");
+                }
+
+                Self::Database(SurrealErr::Api(ApiErr::Query(x)))
+            }
+            x => Self::Database(SurrealErr::Api(x)),
+        }
+    }
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> axum::response::Response {
+        let status = match &self {
+            ApiError::NotFound(_) => StatusCode::NOT_FOUND,
+            ApiError::Conflict(_) => StatusCode::CONFLICT,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
+        (status, self.to_string()).into_response()
+    }
+}
+
 pub mod sync {
     use std::fmt::Debug;
 
