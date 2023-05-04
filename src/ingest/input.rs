f74b732d45a644c2f85993480fbf888f4224258a
Enitoni <5295397+Enitoni@users.noreply.github.com>
2023-05-04 20:15:16 +0200

    Add WaveDistrict track implementation and new InputError type

--- src/ingest/input.rs
+++ src/ingest/input.rs
@@ -1,7 +1,33 @@
+use thiserror::Error;
+
 #[derive(Debug, Clone)]
 pub enum Input {
     YouTube(YouTubeVideo),
     Url(Url),
+}
+
+#[derive(Debug, Error)]
+pub enum InputError {
+    #[error("Input type is supported but resource was not found")]
+    NotFound,
+
+    #[error("Input did not match")]
+    NoMatch,
+
+    #[error("Unsupported input type")]
+    UnsupportedType,
+
+    #[error("Failed to fetch resource")]
+    NetworkFailed,
+
+    #[error("Resource is invalid")]
+    Invalid,
+
+    #[error(transparent)]
+    Other(Box<dyn std::error::Error>),
+
+    #[error("An unknown error occurred")]
+    Unknown,
 }
 
 impl Input {
@@ -82,7 +108,7 @@ use super::loading::Loader;
 mod youtube {
     use std::{
         fmt::Display,
-        io::{self, Read},
+        io::Read,
         process::{Command, Stdio},
     };
 
@@ -277,6 +303,149 @@ mod youtube {
             ));
             assert!(!YouTubeVideo::is_valid_url("https://google.com"));
             assert!(!YouTubeVideo::is_valid_url("kpofkagt"));
+        }
+    }
+}
+
+mod wavedistrict {
+    use std::{fmt::Display, io::Read};
+
+    use fundsp::lazy_static::lazy_static;
+    use hyper::StatusCode;
+    use parking_lot::Mutex;
+    use regex::Regex;
+    use serde::Deserialize;
+
+    use crate::{
+        audio::SAMPLES_PER_SEC,
+        http::stream::ByteRangeStream,
+        ingest::{
+            ffmpeg,
+            loading::{LoadResult, Loader, ProbeResult},
+            SinkLength,
+        },
+    };
+
+    use super::InputError;
+    use reqwest::blocking::Client;
+
+    lazy_static! {
+        static ref REGEX: Regex = Regex::new(
+            r"^(?:https?://)?wavedistrict\.com/@(?P<username>[a-z0-9-]+)/tracks/(?P<slug>[a-z0-9-]+)/?$"
+        ).unwrap();
+    }
+
+    #[derive(Debug, Clone, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    struct Source {
+        ext: String,
+        name: String,
+        streamable: bool,
+        quality_rating: u32,
+    }
+
+    #[derive(Debug, Clone, Deserialize)]
+    struct Media {
+        key: String,
+        sources: Vec<Source>,
+    }
+
+    #[derive(Debug, Clone, Deserialize)]
+    pub struct Track {
+        id: String,
+        title: String,
+        audio: Media,
+    }
+
+    #[derive(Debug)]
+    pub struct TrackLoader {
+        track: Track,
+        stream_url: String,
+        stream: Mutex<ByteRangeStream>,
+    }
+
+    impl Track {
+        pub fn from_url(url: &str) -> Result<Self, InputError> {
+            let (username, slug) = REGEX
+                .captures(url)
+                .and_then(|c| c.name("username").zip(c.name("slug")))
+                .map(|(u, s)| (u.as_str(), s.as_str()))
+                .ok_or(InputError::NoMatch)?;
+
+            let api_url = format!(
+                "https://api.wavedistrict.com/users/{}/tracks/{}",
+                username, slug
+            );
+
+            let track: Track = Client::new()
+                .get(api_url)
+                .send()
+                .map_err(|err| match err.status() {
+                    Some(StatusCode::NOT_FOUND) => InputError::NotFound,
+                    _ => InputError::NetworkFailed,
+                })
+                .and_then(|r| r.json().map_err(|x| InputError::Other(x.into())))?;
+
+            Ok(track)
+        }
+
+        pub fn loader(&self) -> Result<TrackLoader, InputError> {
+            let audio_url = self.audio.best_source_url().ok_or(InputError::Invalid)?;
+
+            let stream = ByteRangeStream::try_new(audio_url.clone()).ok_or(InputError::Unknown)?;
+
+            Ok(TrackLoader {
+                track: self.clone(),
+                stream: stream.into(),
+                stream_url: audio_url,
+            })
+        }
+    }
+
+    impl Media {
+        fn best_source_url(&self) -> Option<String> {
+            let best_source = self
+                .sources
+                .iter()
+                .max_by(|a, b| b.quality_rating.cmp(&a.quality_rating));
+
+            best_source.map(|source| {
+                format!(
+                    "https://media.wavedistrict.com/audio/{}.{}?source={}",
+                    self.key, source.ext, source.name
+                )
+            })
+        }
+    }
+
+    impl Loader for TrackLoader {
+        fn load(&mut self, amount: usize) -> crate::ingest::loading::LoadResult {
+            let mut buf = vec![0; amount];
+
+            let bytes_read = self.stream.lock().read(&mut buf).unwrap_or_default();
+            let bytes: Vec<_> = buf[..bytes_read].to_vec();
+
+            if bytes_read > 0 {
+                LoadResult::Data(bytes)
+            } else {
+                LoadResult::Empty
+            }
+        }
+
+        fn probe(&self) -> Option<crate::ingest::loading::ProbeResult> {
+            ffmpeg::probe(&self.stream_url).map(|probe| {
+                let length_in_samples = (probe.duration * SAMPLES_PER_SEC as f32).floor() as usize;
+
+                ProbeResult {
+                    length: SinkLength::Exact(length_in_samples),
+                }
+            })
+        }
+    }
+
+    impl Display for Track {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.title)
         }
     }
 }
