d54e3e24f7a788e7f1417fb49e23f21c05d04cc8
Enitoni <5295397+Enitoni@users.noreply.github.com>
2023-05-11 16:25:11 +0200

    Change wav streaming to 16bit

--- src/audio/encoding.rs
+++ src/audio/encoding.rs
@@ -1,54 +1,40 @@
 use std::fmt::Debug;
 
-use super::{new::StreamConsumer, SAMPLE_IN_BYTES};
+use super::{new::StreamConsumer, Sample, SAMPLE_IN_BYTES};
 use std::io::Read;
 
 /// Implements streaming a .wav file
 pub struct WaveStream {
     underlying: StreamConsumer,
     did_write_header: bool,
+    header: WaveHeader,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum HeaderValue {
+    Ascii(&'static str),
+    TwoBytes(u16),
+    FourBytes(u32),
+}
+
+struct WaveHeader {
+    channel_count: u16,
+    sample_rate: u32,
+    bit_depth: u16,
 }
 
 impl WaveStream {
-    /// WAVE file header.
-    /// Specifies 2 channel interleaved 32-bit floating point
-    const HEADER: [u8; 44] = [
-        /*
-        // ChunkID: Contains the letters "RIFF" in ASCII form, change last number to 80 if "RIFX" is used
-        82, 73, 70, 70,
-        // ChunkSize: 36 + SubChunk2Size
-        36, 0, 0, 0,
-        // Format: Contains the letters "WAVE"
-        87, 65, 86, 69,
-        // Subchunk1ID: Contains the letters "fmt "
-        102, 109, 116, 32,
-        // Subchunk1Size: 16 for PCM.
-        16, 0, 0, 0,
-        // AudioFormat: PCM = 1
-        1, 0,
-        // NumChannels: Mono = 1, Stereo = 2, etc.
-        2, 0,
-        // SampleRate: 8000, 44100, etc.
-        68, 212, 0, 0,
-        // ByteRate: SampleRate * NumChannels * BitsPerSample/8
-        32, 98, 5, 0,
-        // BlockAlign: == NumChannels * BitsPerSample / 8
-        8, 0,
-        // BitsPerSample: 8 bits = 8, 16 bits = 16, etc.
-        32, 0,
-        // Subchunk2ID: Contains the letters "data"
-        100, 97, 116, 97,
-        // Subchunk2Size: NumSamples * NumChannels * BitsPerSample / 8
-        0, 0, 0, 0
-        */
-        82, 73, 70, 70, 36, 0, 0, 0, 87, 65, 86, 69, 102, 109, 116, 32, 16, 0, 0, 0, 3, 0, 2, 0, 68,
-        172, 0, 0, 32, 98, 5, 0, 8, 0, 32, 0, 100, 97, 116, 97, 0, 0, 0, 0,
-    ];
-
     pub const MIME: &'static str = "audio/wav";
 
     pub fn new(underlying: StreamConsumer) -> Self {
+        let header = WaveHeader {
+            channel_count: super::CHANNEL_COUNT as u16,
+            sample_rate: super::SAMPLE_RATE as u32,
+            bit_depth: 16,
+        };
+
         Self {
+            header,
             underlying,
             did_write_header: false,
         }
@@ -63,7 +49,7 @@ impl Debug for WaveStream {
 
 impl Read for WaveStream {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let header = Self::HEADER;
+        let header = self.header.to_bytes();
         let header_len = header.len();
 
         let mut bytes_written = 0;
@@ -77,11 +63,12 @@ impl Read for WaveStream {
 
         let body_buf = &mut buf[bytes_written..];
 
-        let mut samples = vec![0f32; body_buf.len() / SAMPLE_IN_BYTES];
+        let mut samples = vec![0f32; body_buf.len() / (SAMPLE_IN_BYTES / 2)];
         let amount_of_samples = self.underlying.read(&mut samples);
 
         let samples_in_bytes: Vec<_> = samples[..amount_of_samples]
             .iter()
+            .map(|s| (s * i16::MAX as Sample) as i16)
             .flat_map(|s| s.to_le_bytes())
             .collect();
 
@@ -89,5 +76,73 @@ impl Read for WaveStream {
         bytes_written += samples_in_bytes.len();
 
         Ok(bytes_written)
+    }
+}
+
+impl HeaderValue {
+    fn to_bytes(self) -> Vec<u8> {
+        match self {
+            HeaderValue::Ascii(x) => x.as_bytes().to_vec(),
+            HeaderValue::TwoBytes(x) => x.to_le_bytes().to_vec(),
+            HeaderValue::FourBytes(x) => x.to_le_bytes().to_vec(),
+        }
+    }
+}
+
+impl WaveHeader {
+    // ChunkID: Contains the letters "RIFF" in ASCII form, change last number to 80 if "RIFX" is used
+    const CHUNK_ID: HeaderValue = HeaderValue::Ascii("RIFF");
+
+    // ChunkSize: 36 + SubChunk2Size
+    const CHUNK_SIZE: HeaderValue = HeaderValue::FourBytes(36);
+
+    // Format: Contains the letters "WAVE"
+    const FORMAT: HeaderValue = HeaderValue::Ascii("WAVE");
+
+    // Subchunk1ID: Contains the letters "fmt "
+    const SUBCHUNK_1_ID: HeaderValue = HeaderValue::Ascii("fmt ");
+
+    // Subchunk1Size: 16 for PCM.
+    const SUBCHUNK_1_SIZE: HeaderValue = HeaderValue::FourBytes(16);
+
+    // AudioFormat: PCM = 1
+    const AUDIO_FORMAT: HeaderValue = HeaderValue::TwoBytes(1);
+
+    // Subchunk2ID: Contains the letters "data"
+    const SUBCHUNK_2_ID: HeaderValue = HeaderValue::Ascii("data");
+
+    // Subchunk2Size: NumSamples * NumChannels * BitsPerSample / 8
+    // This is 0 because size is unknown.
+    const SUBCHUNK_2_SIZE: HeaderValue = HeaderValue::FourBytes(0);
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let num_channels = HeaderValue::TwoBytes(self.channel_count);
+        let sample_rate = HeaderValue::FourBytes(self.sample_rate);
+
+        let byte_rate = HeaderValue::FourBytes(
+            self.sample_rate * self.channel_count as u32 * self.bit_depth as u32 / 8,
+        );
+
+        let block_align = HeaderValue::TwoBytes(self.channel_count * self.bit_depth / 8);
+        let bits_per_sample = HeaderValue::TwoBytes(self.bit_depth);
+
+        [
+            Self::CHUNK_ID,
+            Self::CHUNK_SIZE,
+            Self::FORMAT,
+            Self::SUBCHUNK_1_ID,
+            Self::SUBCHUNK_1_SIZE,
+            Self::AUDIO_FORMAT,
+            num_channels,
+            sample_rate,
+            byte_rate,
+            block_align,
+            bits_per_sample,
+            Self::SUBCHUNK_2_ID,
+            Self::SUBCHUNK_2_SIZE,
+        ]
+        .into_iter()
+        .flat_map(HeaderValue::to_bytes)
+        .collect()
     }
 }
