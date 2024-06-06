e6b147b80e492dec1c268a0720f46a922d166d4b
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-06-06 07:46:25 +0200

    add unfinished Playback struct

--- turntable-core/src/lib.rs
+++ turntable-core/src/lib.rs
@@ -1,8 +1,10 @@
 mod config;
 mod ingestion;
+mod playback;
 mod util;
 
 pub mod implementors;
 pub use config::*;
 pub use ingestion::*;
+pub use playback::*;
 pub use util::*;
---
+++ turntable-core/src/playback/mod.rs
@@ -0,0 +1,17 @@
+use std::sync::Arc;
+
+use crate::Ingestion;
+
+/// The playback type is responsible for managing players, processing playback, and preloading sinks as needed.
+pub struct Playback<I> {
+    ingestion: Arc<I>,
+}
+
+impl<I> Playback<I>
+where
+    I: Ingestion,
+{
+    pub fn new(ingestion: Arc<I>) -> Self {
+        Self { ingestion }
+    }
+}
