5ace2e74da88aa4f597d27ffdcf1d8263a5c07d7
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-06-07 06:13:00 +0200

    finish implementation of Output

--- turntable-core/src/output/mod.rs
+++ turntable-core/src/output/mod.rs
@@ -1,11 +1,79 @@
-mod stream;
-pub use stream::*;
+use std::sync::Arc;
 
-mod encoder;
-pub use encoder::*;
+use crate::{Config, PlayerId, Sample};
+use crossbeam::channel::{unbounded, Receiver, Sender};
+use dashmap::DashMap;
 
 mod consumer;
+mod encoder;
+mod stream;
+
 pub use consumer::*;
+pub use encoder::*;
+pub use stream::*;
 
 /// Manages streams for consuming a [Player].
-pub struct Output {}
+pub struct Output {
+    config: Config,
+    streams: Arc<DashMap<PlayerId, Arc<Stream>>>,
+    sample_sender: Sender<ProcessedSamples>,
+}
+
+struct ProcessedSamples {
+    player_id: PlayerId,
+    samples: Vec<Sample>,
+}
+
+impl Output {
+    pub fn new(config: Config) -> Self {
+        let (sample_sender, sample_receiver) = unbounded();
+        let streams = Arc::new(DashMap::new());
+
+        spawn_output_thread(sample_receiver, streams.clone());
+
+        Self {
+            config,
+            streams,
+            sample_sender,
+        }
+    }
+
+    /// Creates a new stream for the given player.
+    pub fn register_player(&self, player_id: PlayerId) {
+        let new_stream = Stream::new(self.config.clone());
+        self.streams.insert(player_id, new_stream);
+    }
+
+    /// Gets a consumer for the associated player, with the given encoder.
+    pub fn consume_player<E>(&self, player_id: PlayerId) -> Arc<Consumer>
+    where
+        E: Encoder,
+    {
+        let stream = self
+            .streams
+            .get(&player_id)
+            .expect("consume_player() is not called with a player that does not exist");
+
+        stream.consume::<E>()
+    }
+
+    /// Pushes samples to the associated player's stream.
+    pub fn push(&self, player_id: PlayerId, samples: Vec<Sample>) {
+        self.sample_sender
+            .send(ProcessedSamples { player_id, samples })
+            .expect("processed samples are sent");
+    }
+}
+
+fn spawn_output_thread(
+    receiver: Receiver<ProcessedSamples>,
+    streams: Arc<DashMap<PlayerId, Arc<Stream>>>,
+) {
+    loop {
+        let processed_samples = receiver.recv().expect("processed samples are received");
+
+        if let Some(stream) = streams.get(&processed_samples.player_id) {
+            stream.push(&processed_samples.samples);
+        }
+    }
+}
