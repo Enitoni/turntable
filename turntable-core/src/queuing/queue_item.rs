dc11965499e6668dae4f1103bb095625a336a6d4
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-06-12 19:36:37 +0200

    allow queue item to receive and return sink ids

--- turntable-core/src/queuing/queue_item.rs
+++ turntable-core/src/queuing/queue_item.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 
-use crate::BoxedLoadable;
+use crate::{BoxedLoadable, SinkId};
 
 /// Represents an item in a queue.
 #[async_trait]
@@ -11,8 +11,15 @@ where
     /// Returns the length of the item in seconds, if known.
     fn length(&self) -> Option<f32>;
 
-    /// Returns the id of the item.
-    fn id(&self) -> String;
+    /// Registers a sink with the item.
+    fn register_sink(&self, sink_id: SinkId);
+
+    /// Returns the sink id of the item, if registered.
+    /// If this returns `None`, the loadable() method will be called to create a new sink.
+    fn sink_id(&self) -> Option<SinkId>;
+
+    /// Returns an id that is used to identify the item for external users of the api
+    fn item_id(&self) -> String;
 
     /// Returns the item's loadable.
     /// This is async because some sources may need to do additional work to create a loadable.
@@ -20,19 +27,27 @@ where
 }
 
 /// [QueueItem] trait object.
-pub type BoxedQueueItem = Box<dyn QueueItem>;
+pub struct BoxedQueueItem(Box<dyn QueueItem>);
 
 #[async_trait]
 impl QueueItem for BoxedQueueItem {
     fn length(&self) -> Option<f32> {
-        self.as_ref().length()
+        self.0.length()
     }
 
-    fn id(&self) -> String {
-        self.as_ref().id()
+    fn register_sink(&self, sink_id: SinkId) {
+        self.0.register_sink(sink_id)
+    }
+
+    fn sink_id(&self) -> Option<SinkId> {
+        self.0.sink_id()
+    }
+
+    fn item_id(&self) -> String {
+        self.0.item_id()
     }
 
     async fn loadable(&self) -> BoxedLoadable {
-        self.loadable().await
+        self.0.loadable().await
     }
 }
