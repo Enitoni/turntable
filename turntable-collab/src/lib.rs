a4b420e3c8924dceb7caa1577c9ef10669afe513
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-06-21 12:50:01 +0200

    add track

--- turntable-collab/src/lib.rs
+++ turntable-collab/src/lib.rs
@@ -1,1 +1,5 @@
 mod input;
+mod track;
+
+pub use input::*;
+pub use track::*;
---
+++ turntable-collab/src/track.rs
@@ -0,0 +1,61 @@
+use async_trait::async_trait;
+use parking_lot::Mutex;
+use std::{error::Error, sync::Arc};
+use turntable_core::{BoxedLoadable, Id, QueueItem, SinkId};
+
+use crate::input::Input;
+
+pub type TrackId = Id<Track>;
+
+/// A single track in a queue
+#[derive(Clone)]
+pub struct Track {
+    pub id: TrackId,
+
+    input: Arc<Input>,
+    state: Arc<Mutex<TrackState>>,
+}
+
+#[derive(Debug, Default, Clone)]
+enum TrackState {
+    #[default]
+    Inactive,
+    Active(SinkId),
+    Error(String),
+}
+
+#[async_trait]
+impl QueueItem for Track {
+    fn length(&self) -> Option<f32> {
+        self.input.length()
+    }
+
+    fn item_id(&self) -> String {
+        self.id.to_string()
+    }
+
+    fn register_sink(&self, sink_id: SinkId) {
+        *self.state.lock() = TrackState::Active(sink_id);
+    }
+
+    fn sink_id(&self) -> Option<SinkId> {
+        match *self.state.lock() {
+            TrackState::Active(sink_id) => Some(sink_id),
+            _ => None,
+        }
+    }
+
+    async fn loadable(&self) -> Result<BoxedLoadable, Box<dyn Error>> {
+        Ok(self.input.loadable().await?)
+    }
+}
+
+impl From<Input> for Track {
+    fn from(input: Input) -> Self {
+        Self {
+            id: TrackId::new(),
+            input: Arc::new(input),
+            state: Default::default(),
+        }
+    }
+}
