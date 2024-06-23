4a866319ac35123884b7b160956291288bd42805
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-06-23 06:12:26 +0200

    Semi-fix LoadableNetworkStream

--- turntable-impls/src/loadables/loadable_network_stream.rs
+++ turntable-impls/src/loadables/loadable_network_stream.rs
@@ -2,8 +2,9 @@ use std::{error::Error, io::SeekFrom};
 
 use async_trait::async_trait;
 use crossbeam::atomic::AtomicCell;
+use parking_lot::Mutex;
 use reqwest::Client;
-use turntable_core::{assign_slice, Loadable, LoaderLength, ReadResult};
+use turntable_core::{assign_slice, assign_slice_with_offset, Loadable, LoaderLength, ReadResult};
 
 /// A loadable that reads from a network stream.
 /// If the stream supports byte ranges, it can be seeked.
@@ -11,12 +12,17 @@ pub struct LoadableNetworkStream {
     url: String,
     client: Client,
     length: Option<usize>,
-    offset: AtomicCell<usize>,
     supports_byte_ranges: bool,
+    read_offset: AtomicCell<usize>,
+    /// A cache of bytes that are preloaded to prevent spamming the network.
+    /// This is because the ingestion may request very small amounts of data at a time.
+    loaded_bytes: Mutex<Vec<u8>>,
+    loaded_bytes_offset: AtomicCell<usize>,
 }
 
 impl LoadableNetworkStream {
-    const MAX_CHUNK_SIZE: usize = 500000;
+    const MAX_CHUNK_SIZE: usize = 5_000_000; // 5MB
+    const MIN_CHUNK_SIZE: usize = 100_000; // 100KB
 
     pub async fn new<S>(url: S) -> Result<Self, reqwest::Error>
     where
@@ -41,25 +47,23 @@ impl LoadableNetworkStream {
             url,
             client,
             length,
-            offset: Default::default(),
             supports_byte_ranges,
+            read_offset: Default::default(),
+            loaded_bytes: Default::default(),
+            loaded_bytes_offset: Default::default(),
         })
     }
-}
 
-#[async_trait]
-impl Loadable for LoadableNetworkStream {
-    async fn read(&self, buf: &mut [u8]) -> Result<ReadResult, Box<dyn Error>> {
-        let amount = buf.len().min(Self::MAX_CHUNK_SIZE);
-
-        let start = self.offset.load();
-        let end = (start + amount).min(self.length.unwrap_or(usize::MAX));
+    /// Loads an amount of bytes from the current load offset.
+    async fn load(&self, amount: usize) -> Result<(), Box<dyn Error>> {
+        // Set up the offsets that we will load from and to.
+        let start = self.loaded_bytes_offset.load();
+        let end = (start + amount).min(self.normal_len()).saturating_sub(1);
 
         let mut request = self.client.get(&self.url);
 
         if self.supports_byte_ranges {
-            let safe_end = end.saturating_sub(1);
-            let range = format!("bytes={start}-{safe_end}");
+            let range = format!("bytes={}-{}", start, end);
             request = request.header("Range", range);
         }
 
@@ -69,10 +73,80 @@ impl Loadable for LoadableNetworkStream {
             return Err(format!("Request failed with status code {}", response.status()).into());
         }
 
-        let bytes = response.bytes().await?;
-        let amount_written = assign_slice(&bytes, buf);
+        let new_bytes = response.bytes().await?;
+        let mut current_bytes = self.loaded_bytes.lock();
 
-        let result = if amount_written == amount {
+        current_bytes.extend_from_slice(&new_bytes);
+        self.loaded_bytes_offset.store(end + 1);
+
+        Ok(())
+    }
+
+    /// Ensures that the given amount of bytes at the given offset are preloaded from the network.
+    async fn ensure_preloaded(&self, requested_amount: usize) -> Result<(), Box<dyn Error>> {
+        let current_read_offset = self.read_offset.load();
+        let current_load_offset = self.loaded_bytes_offset.load();
+        let current_loaded_len = self.loaded_bytes.lock().len();
+
+        // This number is relative to the vector
+        let new_read_offset = current_read_offset + requested_amount;
+        let remaining_bytes = self
+            .length
+            .unwrap_or(usize::MAX)
+            .saturating_sub(current_load_offset);
+
+        // Skip preloading if we have enough data for the read, or if there's no more data to read
+        if new_read_offset <= current_loaded_len || remaining_bytes == 0 {
+            return Ok(());
+        }
+
+        // Preload at least MIN_CHUNK_SIZE if possible, but not more than the remaining bytes
+        let minimum_requested_amount = requested_amount
+            .max(Self::MIN_CHUNK_SIZE)
+            .min(remaining_bytes);
+
+        self.load(minimum_requested_amount).await?;
+        Ok(())
+    }
+
+    fn normal_len(&self) -> usize {
+        self.length.unwrap_or(usize::MAX)
+    }
+
+    fn loaded_length(&self) -> usize {
+        self.loaded_bytes.lock().len()
+    }
+
+    fn absolute_load_start_offset(&self) -> usize {
+        self.loaded_bytes_offset
+            .load()
+            .saturating_sub(self.loaded_length())
+    }
+
+    fn absolute_read_offset(&self) -> usize {
+        self.absolute_load_start_offset() + self.read_offset.load()
+    }
+}
+
+#[async_trait]
+impl Loadable for LoadableNetworkStream {
+    async fn read(&self, buf: &mut [u8]) -> Result<ReadResult, Box<dyn Error>> {
+        let amount_to_read = buf.len().min(Self::MAX_CHUNK_SIZE);
+
+        self.ensure_preloaded(amount_to_read).await?;
+
+        let loaded_bytes = self.loaded_bytes.lock();
+        let read_offset = self.read_offset.load();
+
+        let safe_read_start = read_offset.min(loaded_bytes.len());
+        let read_bytes = &loaded_bytes[safe_read_start..];
+
+        let amount_written = assign_slice(read_bytes, buf);
+        let new_read_offset = read_offset + amount_written;
+
+        self.read_offset.store(new_read_offset);
+
+        let result = if amount_written == buf.len() {
             ReadResult::More(amount_written)
         } else {
             ReadResult::End(amount_written)
@@ -86,17 +160,20 @@ impl Loadable for LoadableNetworkStream {
     }
 
     async fn seek(&self, seek: SeekFrom) -> Result<usize, Box<dyn Error>> {
-        let offset = self.offset.load();
+        let offset = self.absolute_read_offset();
 
         let new_offset = match seek {
             SeekFrom::Current(increment) => (increment + offset as i64) as usize,
             SeekFrom::Start(offset) => offset as usize,
-            SeekFrom::End(increment) => {
-                (self.length.unwrap_or(usize::MAX) as i64 + increment) as usize
-            }
+            SeekFrom::End(increment) => (self.normal_len() as i64 + increment) as usize,
         };
 
-        self.offset.store(new_offset);
-        Ok(new_offset)
+        let safe_new_offset = new_offset.min(self.normal_len());
+
+        self.read_offset.store(0);
+        self.loaded_bytes_offset.store(safe_new_offset);
+        self.loaded_bytes.lock().clear();
+
+        Ok(safe_new_offset)
     }
 }
