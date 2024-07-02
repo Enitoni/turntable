21fe6fb2eedfdf005a2757583013207551cbc590
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-07-02 18:15:32 +0200

    add FromRequestParts impl for ServerContext

--- turntable-server/src/context.rs
+++ turntable-server/src/context.rs
@@ -1,9 +1,27 @@
-use std::sync::Arc;
+use std::{convert::Infallible, sync::Arc};
 
-use axum::extract::FromRef;
+use axum::{
+    async_trait,
+    extract::{FromRef, FromRequestParts},
+    http::request::Parts,
+};
 use turntable_collab::Collab;
 
 #[derive(Clone, FromRef)]
 pub struct ServerContext {
     pub collab: Arc<Collab>,
+}
+
+#[async_trait]
+impl FromRequestParts<ServerContext> for ServerContext {
+    type Rejection = Infallible;
+
+    async fn from_request_parts(
+        _parts: &mut Parts,
+        state: &ServerContext,
+    ) -> Result<Self, Self::Rejection> {
+        let context = ServerContext::from_ref(state);
+
+        Ok(context)
+    }
 }
