039bb9954af36e264692e0f05489dd73a31983a2
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-07-05 01:19:03 +0200

    set up collab event system

---
+++ turntable-collab/src/events.rs
@@ -0,0 +1,110 @@
+use crossbeam::channel::{Receiver, Sender};
+use turntable_core::{PipelineEvent, PlayerState};
+
+use crate::{CollabContext, LinearQueueItem, PrimaryKey, RoomMemberData, TrackId};
+
+pub type EventSender = Sender<CollabEvent>;
+pub type EventReceiver = Receiver<CollabEvent>;
+
+/// Events emitted by
+pub enum CollabEvent {
+    /// A player's state has changed.
+    PlayerStateUpdate {
+        room_id: PrimaryKey,
+        new_state: PlayerState,
+    },
+    /// A player's playback offset has changed.
+    PlayerTimeUpdate {
+        room_id: PrimaryKey,
+        /// The current position of the player, in seconds.
+        position: f32,
+        /// The total position of the player, in seconds.
+        total_position: f32,
+    },
+    /// A track as a queue item has been ingested
+    TrackActivated {
+        /// The id of the player the queue item's queue belongs to.
+        room_id: PrimaryKey,
+        /// The id of the new sink created for the queue item.
+        track_id: TrackId,
+    },
+    /// A track as a queue item failed to be ingested.
+    TrackActivationError {
+        /// The id of the player the queue item's queue belongs to.
+        room_id: PrimaryKey,
+        /// The id of the new sink created for the queue item.
+        track_id: TrackId,
+        /// The error that happened while activating the queue item.
+        error: String,
+    },
+    /// The currently playing track of a room updated
+    RoomQueueItemUpdate {
+        room_id: PrimaryKey,
+        new_item: Option<LinearQueueItem>,
+    },
+    /// A queue was modified and updated
+    RoomQueueUpdate {
+        history: Vec<LinearQueueItem>,
+        items: Vec<LinearQueueItem>,
+    },
+    /// User become a member of a room
+    UserJoined {
+        room_id: PrimaryKey,
+        new_member: RoomMemberData,
+    },
+    /// User left a room
+    UserLeft {
+        room_id: PrimaryKey,
+        member_id: PrimaryKey,
+    },
+    /// A user connected with a stream key to a room
+    UserConnected {
+        room_id: PrimaryKey,
+        user_id: PrimaryKey,
+        source: String,
+    },
+    /// A user disconnected from a room
+    UserDisconnected {
+        room_id: PrimaryKey,
+        user_id: PrimaryKey,
+        source: String,
+    },
+}
+
+impl CollabEvent {
+    /// Convert a pipeline event to a friendly collab event
+    pub fn from_pipeline_event(
+        context: &CollabContext,
+        event: PipelineEvent,
+    ) -> Option<CollabEvent> {
+        match event {
+            PipelineEvent::PlayerStateUpdate {
+                player_id,
+                new_state,
+            } => context
+                .room_by_player_id(player_id)
+                .map(|room| Self::PlayerStateUpdate {
+                    room_id: room.id(),
+                    new_state,
+                }),
+            PipelineEvent::PlayerTimeUpdate {
+                player_id,
+                position,
+                total_position,
+            } => context
+                .room_by_player_id(player_id)
+                .map(|room| Self::PlayerTimeUpdate {
+                    room_id: room.id(),
+                    position,
+                    total_position,
+                }),
+            PipelineEvent::PlayerAdvanced { player_id } => context
+                .room_by_player_id(player_id)
+                .map(|room| Self::RoomQueueItemUpdate {
+                    room_id: room.id(),
+                    new_item: room.current_item(),
+                }),
+            _ => None,
+        }
+    }
+}
--- turntable-collab/src/lib.rs
+++ turntable-collab/src/lib.rs
@@ -8,8 +8,10 @@ mod track;
 mod util;
 
 use auth::Auth;
+use crossbeam::channel::unbounded;
+use events::{CollabEvent, EventReceiver, EventSender};
 use rooms::{RoomId, RoomManager};
-use std::sync::Arc;
+use std::{sync::Arc, thread};
 
 pub use auth::{AuthError, Credentials, NewPlainUser};
 pub use db::*;
@@ -26,18 +28,19 @@ pub type CollabDatabase = PgDatabase;
 
 /// The turntable collab system, facilitating room management, authentication, and more.
 pub struct Collab {
-    pipeline: Arc<CollabPipeline>,
-    database: Arc<CollabDatabase>,
+    event_receiver: EventReceiver,
 
     pub auth: Auth<CollabDatabase>,
     pub rooms: RoomManager,
 }
 
 /// A type passed to various components of the collab system, to access state, emit events, and dispatch actions.
+#[derive(Clone)]
 pub struct CollabContext {
+    event_sender: EventSender,
+
     pub pipeline: Arc<CollabPipeline>,
     pub database: Arc<CollabDatabase>,
-
     pub rooms: ArcedStore<RoomId, Room>,
 }
 
@@ -48,12 +51,14 @@ impl Collab {
                 .await
                 .expect("database is created"),
         );
+
         let pipeline = Arc::new(CollabPipeline::new(config));
+        let (event_sender, event_receiver) = unbounded();
 
         let context = CollabContext {
             database: database.clone(),
             pipeline: pipeline.clone(),
-
+            event_sender: event_sender.clone(),
             rooms: Default::default(),
         };
 
@@ -62,10 +67,11 @@ impl Collab {
 
         let new = Self {
             auth,
-            pipeline,
-            database,
+            event_receiver,
             rooms: room_manager,
         };
+
+        spawn_pipeline_event_conversion_thread(&context, &event_sender);
 
         new.init().await;
         new
@@ -75,19 +81,20 @@ impl Collab {
     async fn init(&self) {
         self.rooms.restore().await.expect("rooms are restored");
     }
-}
 
-impl Clone for CollabContext {
-    fn clone(&self) -> Self {
-        Self {
-            database: self.database.clone(),
-            pipeline: self.pipeline.clone(),
-            rooms: self.rooms.clone(),
-        }
+    /// Receive events from the collab.
+    pub fn wait_for_event(&self) -> CollabEvent {
+        self.event_receiver
+            .recv()
+            .expect("event is received without error")
     }
 }
 
 impl CollabContext {
+    pub fn emit(&self, event: CollabEvent) {
+        self.event_sender.send(event).expect("event is sent");
+    }
+
     /// Gets a room by its player id if it exists and is active
     pub fn room_by_player_id(&self, player_id: PlayerId) -> Option<Arc<Room>> {
         self.rooms
@@ -95,4 +102,19 @@ impl CollabContext {
             .find(|r| r.player().ok().filter(|p| p.id == player_id).is_some())
             .map(|r| r.clone())
     }
+}
+
+fn spawn_pipeline_event_conversion_thread(context: &CollabContext, sender: &EventSender) {
+    let context = context.to_owned();
+    let sender = sender.to_owned();
+
+    let run = move || loop {
+        let event = context.pipeline.wait_for_event();
+
+        if let Some(converted_event) = CollabEvent::from_pipeline_event(&context, event) {
+            sender.send(converted_event).expect("event is sent")
+        }
+    };
+
+    thread::spawn(run);
 }
