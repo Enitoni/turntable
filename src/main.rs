ed7b4999a922e6536c7870d4e71182a1ef61dea2
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-07-06 18:01:10 +0200

    set up dotenv for database_url

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{env, sync::Arc};
 
 use turntable_collab::Collab;
 use turntable_core::Config;
@@ -6,15 +6,13 @@ use turntable_server::run_server;
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
     println!("Setting up Collab...");
 
-    let collab = Arc::new(
-        Collab::new(
-            Config::default(),
-            "postgres://turntable:turntable@localhost/turntable",
-        )
-        .await,
-    );
+    let collab = Arc::new(Collab::new(Config::default(), &database_url).await);
 
     println!("Server running.");
     run_server(&collab).await
