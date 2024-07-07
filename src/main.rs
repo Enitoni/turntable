0faac083153fd262021ac7e14b6032ef88808977
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-07-08 00:03:16 +0200

    remove unnecessary logs

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,11 @@ async fn main() {
     logging::init_logger();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    println!("Setting up Collab...");
-
     let collab = Arc::new(Collab::new(Config::default(), &database_url).await);
 
     let port = env::var("TURNTABLE_SERVER_PORT")
         .map(|x| x.parse::<u16>().expect("Port must be a number"))
         .unwrap_or(DEFAULT_PORT);
 
-    println!("Server running on http://localhost:{}", port);
     run_server(&collab, port).await
 }
